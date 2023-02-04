6cbdf5ecd1a1dedc55d7dadccc8883edbab010c8
wjdals95 <wjdals992@naver.com>
2023-02-04 19:18:15 +0900

    작업에 페이로드 연결 & 여러 State 속성 작업

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,24 +11,29 @@ const Counter = () => {
   //useSelector 훅을 사용하면 react-redux는 이 컴포넌트를 위해
   //리덕스 저장소에 자동으로 구독을 설정한다.
   const counter = useSelector((state) => state.counter);
-
+  const show = useSelector((state) => state.showCounter);
   const incrementHandler = () => {
     dispatch({ type: "increment" });
     // type은 Redux store reducer에서 사용하는 identifiers 중 하나이어야 한다.
   };
-
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 }); // amount 페이로드 추가
+  };
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show &&<div className={classes.value}>{counter}</div>}
       <dir>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </dir>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,35 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
-      counter: state.counter + 1,
+      counter: state.counter + 1, 
+      //리덕스에서는 기존의 상태값을 절대 변경해서는 안된다.
+      //그러므로 기존의 값을 복사한 새로운 객체나 배열을 생성하여 사용한다.
+      showCounter: state.showCounter,
     };
   }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
     };
   }
   return state;
