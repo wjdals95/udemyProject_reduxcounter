4f509ec7ca0d2ef85fdd2785fc2bddcb904e370d
wjdals95 <wjdals992@naver.com>
2023-02-04 18:53:25 +0900

    클래스기반 컴포넌트에서 리덕스 사용법

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,9 @@
 import { useSelector, useDispatch } from "react-redux";
 
+//class컴포넌트에서 리덕스를 쓸 때 사용하는 훅
+// import{connect} from 'react-redux'
+//import { Component } from "react";
+
 import classes from "./Counter.module.css";
 
 const Counter = () => {
@@ -9,13 +13,13 @@ const Counter = () => {
   const counter = useSelector((state) => state.counter);
 
   const incrementHandler = () => {
-    dispatch({type: 'increment'}) 
+    dispatch({ type: "increment" });
     // type은 Redux store reducer에서 사용하는 identifiers 중 하나이어야 한다.
-  }
+  };
 
   const decrementHandler = () => {
-    dispatch({type: 'decrement'})
-  }
+    dispatch({ type: "decrement" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -33,3 +37,44 @@ const Counter = () => {
 };
 
 export default Counter;
+
+// //class컴포넌트에서 리덕스 사용하기
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <dir>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </dir>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
